ab892dbe1d97ed6079e52b9d768df96d2d42d994
MuSkAt1337 <tuzan58@gmail.com>
2018-12-19 16:16:44 +0300

    1 comm

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-    Console.WriteLine('Nu zdarova Vladik');  blya srar
+    Console.WriteLine('Nu zdarova Vladik'); 
       
         }
     }
